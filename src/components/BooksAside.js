07f1778c15df6cf2a6d84c5c0150c24bde97de1e
leslieyi <leslieyi411@gmail.com>
2021-08-02 18:44:20 -0400

    new Component

--- src/components/BooksAside.js
+++ src/components/BooksAside.js
@@ -1,44 +1,10 @@
-import { useState } from "react";
+import UserInfo from "./UserInfo"
 
 function BooksAside({ userData }) {
-  const [clicked, setClicked] = useState(false);
-  function handleClick() {
-    setClicked(!clicked);
-  }
 
   return (
     <div className="column-left">
-      <h3>{userData.name}'s Profile</h3>
-      <ul>
-        <li>My Favorite Genre: {userData.favoriteGenre}</li>
-        {userData.booksInfo.map((book) => (
-          <div>
-            <br />
-            <img
-              className="bookImage"
-              alt={book.title}
-              src={book.image}
-              onClick={handleClick}
-            />
-            {clicked ? (
-              <div className="books-aside-lists-back">
-                <li>{book.starReview}</li>
-                <br />
-                <li>{book.review}</li>
-                <br />
-                <br />
-              </div>
-            ) : (
-              <div className="books-aside-lists-front">
-                <li>Title: {book.title}</li>
-                <li>Authors: {book.authors}</li>
-                <li>Publisher: {book.publisher}</li>
-                <br />
-              </div>
-            )}
-          </div>
-        ))}
-      </ul>
+        <UserInfo userData={userData}/>
     </div>
   );
 }
