3c1b3c06e4c00910d1745cb71bae737ce5a037f9
Alec Magnet <amagnet@gmail.com>
2021-08-02 15:03:33 -0400

    fart

--- src/components/BookClubAside.js
+++ src/components/BookClubAside.js
@@ -1,6 +1,6 @@
 function BookClubAside() {
     return (
-        <h1>Book club info, message board, if you're not show availabe bookclub info based on your location, two components</h1>
+        <h1>Book club info, message board, if you're not show availabe bookclub info based on your location, two components, FART</h1>
     )
 }
 
