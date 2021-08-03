601cdb822c5683f41d8d4dc33da15fb2c65c8e2e
leslieyi <leslieyi411@gmail.com>
2021-08-03 14:56:31 -0400

    Yearim day 2, 2pm

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,41 @@
 // import '../App.css';
 import { Route, Switch } from "react-router-dom";
-import AppHome from "./AppHome"
-import ClubsPage from "./ClubsPage"
-
+import { useState, useEffect } from "react";
+import AppHome from "./AppHome";
+import ClubsPage from "./ClubsPage";
+import NavBar from "./NavBar";
+import Profiles from "./Profiles";
 
 function App() {
+  const [profileData, setProfileData] = useState([
+    {booksInfo: []}
+  ]);
+  useEffect(() => {
+    fetch("http://localhost:3001/profile")
+      .then((response) => response.json())
+      .then((Data) => {
+        setProfileData(Data);
+      });
+  }, []);
+
   return (
     <div>
-      {/* <Navbar setIsLoggedIn={setIsLoggedIn} /> */}
-
-
+      <NavBar />
       <Switch>
         <Route exact path="/">
-          <AppHome />
+          <AppHome profileData={profileData}/>
+        </Route>
+
+        <Route path="/profiles">
+          <Profiles profileData={profileData} />
         </Route>
 
         <Route path="/clubs">
           <ClubsPage />
         </Route>
       </Switch>
-
-
-
-
     </div>
   );
-
-
 }
 
 export default App;
