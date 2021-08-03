968aa2d2e27a71f5d06475e863212b2f684016af
Alec Magnet <amagnet@gmail.com>
2021-08-03 16:02:39 -0400

    change bookClub and bookClub aside format

--- src/components/BookClubAside.js
+++ src/components/BookClubAside.js
@@ -13,18 +13,17 @@ function BookClubAside({ clubsData }) {
     return (
         <div className="column-right">
             <h1>My Book Clubs</h1>
-            <em>GOALS: (1) render each club as a card instead of a div (2) make each member name a link to their profile (3) add a link to join more clubs (4) make comments each a card and add time and date posted (5) make zip codes render as city names</em>
-            {/* <em>Book club info, message board, if you're not show availabe bookclub info based on your location, two components</em> */}
-            <h3>* * * Add a Link: Create New Club! * * *</h3>
+            <button>Create New Club</button>
+            <span>  </span>
+            <button>Find More Clubs</button>
              {clubsData.length === 0 ?    
                      <div>
                          <h2>You don't belong to any bookclubs yet.</h2>
-                         <p>Would you like to join one?</p> 
-                         {/* make this <p> a link to suggestions */}
+                         <p>Would you like to join one? **make this P a link to suggestions**</p> 
                      </div>
              : <div>{clubsToRender}</div>
              }
-            <h3>* * * Add a Link: Find More Clubs! * * *</h3>
+            <em>GOALS: (1) render each club as a card instead of a div (2) make each member name a link to their profile (3) add a link to join more clubs (4) make comments each a card and add time and date posted (5) make zip codes render as city names</em>
 
         </div>
     )
