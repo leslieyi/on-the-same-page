03fbdebb63c86a87b902ef5f102a5bd6e202cb34
leslieyi <leslieyi411@gmail.com>
2021-08-03 15:51:14 -0400

    ;s

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -3,7 +3,7 @@ import ProfileInfoCard from "./ProfileInfoCard";
 function Profiles({ profileData }) {
   return (
     <div className="profile">
-      <h1>All User's Info</h1>
+      <h1>All Users' Info</h1>
 
       {profileData.map((profile) => (
         <ProfileInfoCard profile={profile} />
