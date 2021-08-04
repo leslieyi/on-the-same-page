81aead2d46b5e3344dc967b70bc88e517874babd
Mari Taksa <welch.mari@gmail.com>
2021-08-04 11:51:31 -0500

    styling

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -7,19 +7,18 @@ function Container ({ profileData }) {
             build out functions to grab the activity and put in this container.*/}
 
             {/* new user, similar author interest or history */}
-            <h3>{profileData[0].name} just joined OTSP!<br></br>{profileData[0].favoriteGenre === profileData[3].favoriteGenre ? ` They like ${profileData[3].favoriteGenre}, too.` : ""} <br></br>Check out their bookshelf.</h3>
+            <p>{profileData[0].name} just joined OTSP!{profileData[0].favoriteGenre === profileData[3].favoriteGenre ? ` They like ${profileData[3].favoriteGenre}, too. Check out their bookshelf.` : ""}</p>
 
             {/* user lives nearby and has their location public */}
-            <h3>{profileData[4].name} just joined OTSP!
-            {profileData[4].location.public === true && profileData[4].area === profileData[3].area ? ` They live in ${profileData[3].area}, too. Maybe the two of you could start a book club!` : ""} Check out their bookshelf.</h3>
+            <p>{profileData[4].name} just joined OTSP!
+            {profileData[4].location.public === true && profileData[4].area === profileData[3].area ? ` They live in ${profileData[3].area}, too. Maybe the two of you could start a book club!` : ""} Check out their bookshelf.</p>
 
             {/* users have location public, live within x miles, and have similarities. utilize zip code api and maybe google api */}
             {/* <h2>Four friends within ten miles like GENRE and GENRE. What about forming a book club and reading TITLE next? You could meet at LOCATION.</h2> */}
 
-            {/* add conditional styling to private entries */}
-            <h3>You added: {profileData[3].booksInfo.filter(a => a.public === true).map(a => <li>{a.title}</li>)} to your public bookshelf.</h3>
+            <p>You added: {profileData[3].booksInfo.filter(a => a.public === true).map(a => <li>{a.title}</li>)} to your public bookshelf.</p>
 
-            <h3 style={{color:"red"}}>You added {profileData[3].booksInfo.filter(a => a.public === false).map(a => <li>{a.title}</li>)} to your private bookshelf.</h3>
+            <p style={{color:"#cb4154"}}>You added: {profileData[3].booksInfo.filter(a => a.public === false).map(a => <li>{a.title}</li>)} to your private bookshelf.</p>
         </div>
 
     )
