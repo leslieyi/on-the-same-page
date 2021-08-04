d6018c42d13b2372e4e048ba12fda21af8c5ec54
leslieyi <leslieyi411@gmail.com>
2021-08-04 12:06:33 -0400

    final

--- src/components/App.js
+++ src/components/App.js
@@ -19,6 +19,8 @@ function App() {
       });
   }, []);
 
+
+
   return (
     <div className="app-body" >
       <NavBar />
--- src/components/ProfileShow.js
+++ src/components/ProfileShow.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Card, Image } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
+
 
 // Here we add `match` to the arguments so we can access the path information in `routerProps` that is passed from Profiles.js
 function ProfileShow({ profileData }) {
@@ -12,31 +13,36 @@ function ProfileShow({ profileData }) {
   // In the initial state, profileData is an empty array and profile is null
   // as a result. Only after fetch, do we get data in profileData.
 
+
   return profile ? (
-    <Card.Group  style={{justifyContent: "center"}}>
+    <Card.Group style={{ justifyContent: "center" }}>
       <Card
         style={{
-          backgroundColor: "	rgb(64,64,64)"
+          backgroundColor: "	rgb(64,64,64)",
           // backgroundImage: `url(https://images.unsplash.com/photo-1535905496755-26ae35d0ae54?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80)`,
           // backgroundSize: "cover",
         }}
       >
         <Image
           src={profile.profileImage}
-          style={{height: "300px"}}
+          style={{ height: "300px" }}
           size="large"
         />
         <Card.Content style={{ color: "white" }}>
           <h1>{profile.name}</h1>
           <span className="span-titles">Favorite Genre:</span>{" "}
           {profile.favoriteGenre}
-          <Card.Description  style={{ color: "white" }}>
-            <span className="span-titles">Area:</span> {profile.area}
+          <Card.Description style={{ color: "white" }}>
+            <span className="span-titles">Area:</span> <p>{profile.area}
+              </p>
             <br />
             <span className="span-titles">Zipcode:</span>{" "}
+            <p>
             {profile.location.public
               ? profile.location.zipcode
               : "❗Sorry, This Info ain't Public❗"}
+              
+            </p>
           </Card.Description>
         </Card.Content>
         <Card.Content extra style={{ color: "white" }}>
@@ -44,7 +50,7 @@ function ProfileShow({ profileData }) {
           {profile.booksInfo.map((book) => (
             <li>{book.title}</li>
           ))}
-          <br/>
+          <br />
         </Card.Content>
       </Card>
     </Card.Group>
