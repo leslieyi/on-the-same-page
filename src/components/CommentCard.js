eeb7626298de5872bdf30e258dd196e0b91ae6d0
Alec Magnet <amagnet@gmail.com>
2021-08-04 13:25:41 -0400

    still messing with dates in CommendCard

--- src/components/CommentCard.js
+++ src/components/CommentCard.js
@@ -1,24 +1,17 @@
 import { Card, Feed, } from 'semantic-ui-react'
 
 function CommentCard({ comment }) {
-	//console.log('Hi from CommentCard', comment)
 	const { poster, time, text} = comment
 
-	const newDate = new Date()
-	const timeAgo = Math.abs(newDate - time)
-	const daysAgo = timeAgo/(1000 * 3600 * 24)
-	console.log("Comment card time:", {time}, "newDate:", {newDate}, "timeAgo:", {timeAgo}, "daysAgo:", {daysAgo})
+	const timeNow = new Date().getTime() / 1000
+	const timeDisplay = parseInt((timeNow - time)/(3600 * 24))
 
 	return(
-		// <div style={{paddingBottom: "10px"}}>
-		// 	<b>{poster}: </b>{text} 
-		// 	{/* <small text-color="light-gray"> at {time}</small> */}
-		// </div>
 		<Feed.Event>
           	<Feed.Label image="https://i.pinimg.com/originals/49/da/b2/49dab23327cf7e9a71ef5b0b57aa3b24.png" />
           	<Feed.Content>
 				<Feed.Summary style={{fontSize: "16px"}} >{poster}</Feed.Summary>
-				<Feed.Date content={`${daysAgo} day ago`} />
+				<Feed.Date content={`${timeDisplay} day ago`} />
 				<Feed.Summary style={{fontWeight: "normal"}} >
 					{text}
 				</Feed.Summary>
