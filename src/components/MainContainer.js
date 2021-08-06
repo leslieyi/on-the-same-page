57ad2e88d971005f6153f7ad9d29e5f2c75e0c37
Mari Taksa <welch.mari@gmail.com>
2021-08-06 09:10:37 -0500

    mari friday

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,44 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import StatusForm from "./StatusForm";
 
-function Container ({ profileData, setProfileData }) {
-    const [newStatus, setNewStatus] = useState("")
-
-    function handleSubmit(e) {
-    e.preventDefault()
-    fetch("http://localhost:3001/user", {
-      method: "POST",
-      headers: {"Content-type" : "application/json"},
-      body: JSON.stringify({newStatus})
-    })
-    .then (resp => resp.json())
-    .then (newStatus => setNewStatus(newStatus))
-  }
-
-    
-    
+function Container ({ profileData }) {
+  
     return (
         <div className="column-center">
-            <StatusForm newStatus={newStatus} setNewStatus={setNewStatus} handleNewStatus={handleNewStatus} handleSubmit={handleSubmit}/>
+            {/* <StatusForm /> */}
 
             <h1>Latest Activity</h1>
-            {/* this container will have 7 h2s. each one will show the latest activity relative to their position. h2#1 will show the latest activity. h2#2 will show the 2nd latest activity. 
-            
-            build out functions to grab the activity and put in this container.*/}
 
             {/* new user, similar author interest or history */}
-            <p>{profileData[0].name} just joined OTSP!{profileData[0].favoriteGenre === profileData[3].favoriteGenre ? ` They like ${profileData[3].favoriteGenre}, too. Check out their bookshelf.` : ""}</p>
+            <h3><a href="./profiles/2">{profileData[1].name}</a> just joined OTSP!</h3><p>{profileData[1].favoriteGenre === profileData[3].favoriteGenre ? ` They like ${profileData[3].favoriteGenre}, too. Check out their bookshelf.` : ""}</p>
 
             {/* user lives nearby and has their location public */}
-            <p>{profileData[4].name} just joined OTSP!
-            {profileData[4].location.public === true && profileData[4].area === profileData[3].area ? ` They live in ${profileData[3].area}, too. Maybe the two of you could start a book club!` : ""} Check out their bookshelf.</p>
+            <h3><a href="./profiles/5">{profileData[4].name}</a> just joined OTSP!</h3><p>
+            {profileData[4].location.public === true && profileData[4].area === profileData[3].area ? ` They live in ${profileData[3].area}, too. Maybe the two of you could start a book club!` : ""}</p>
 
             {/* users have location public, live within x miles, and have similarities. utilize zip code api and maybe google api */}
             {/* <h2>Four friends within ten miles like GENRE and GENRE. What about forming a book club and reading TITLE next? You could meet at LOCATION.</h2> */}
 
-            <p>You added: {profileData[3].booksInfo.filter(a => a.public === true).map(a => <li>{a.title}</li>)} to your public bookshelf.</p>
+            <h3>You added to your public bookshelf:</h3><p>{profileData[3].booksInfo.filter(a => a.public === true).map(a => <li>{a.title}</li>)}</p>
 
-            <p style={{color:"#cb4154"}}>You added: {profileData[3].booksInfo.filter(a => a.public === false).map(a => <li>{a.title}</li>)} to your private bookshelf.</p>
+            <h3 style={{color:"#cb4154"}}>You added to your private bookshelf:</h3><p style={{color:"#cb4154"}}>{profileData[3].booksInfo.filter(a => a.public === false).map(a => <li>{a.title}</li>)}</p>
         </div>
 
     )
--- src/components/StatusForm.js
+++ src/components/StatusForm.js
@@ -1,19 +1,29 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function StatusForm({ setStatus, setNewStatus }) {
-    // function handleNewStatus(newStatus){
-    //     <h1>hi</h1>
-    // }
+// function StatusForm() {
+
+//   const [newStatus, setNewStatus] = useState([])
+
+//   function handleSubmit(e) {
+//     e.preventDefault()
+//     fetch("http://localhost:3001/profile/4", {
+//       method: "POST",
+//       headers: {"Content-type" : "application/json"},
+//       body: JSON.stringify({newStatus})
+//     })
+//     .then (resp => resp.json())
+//     .then (newStatus => setNewStatus(newStatus))
+//   }
   
-  return (
-    <>
-    <h3>New Status</h3>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Let it out, honey..." value={newStatus} onChange={(e) => {setNewStatus(e.target.value)}}/>
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  );
-}
+//   return (
+//     <>
+//     <h3>New Status</h3>
+//       <form onSubmit={handleSubmit}>
+//         <input type="text" name="name" placeholder="Let it out, honey..." value={newStatus} onChange={(e) => {setNewStatus(e.target.value)}}/>
+//         <button type="submit">Submit</button>
+//       </form>
+//     </>
+//   );
+// }
 
-export default StatusForm;
+// export default StatusForm;
